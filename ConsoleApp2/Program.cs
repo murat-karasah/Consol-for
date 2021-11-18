24987f3e1523a3d7ba06d4124cdacdbec7c9dac9
Sabahci <Sabahci@DESKTOP-O6O0BMG>
2021-11-18 10:56:38 +0300

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Lütfen seçiminizi yapınız! Artan için Y azalan için N tuşuna basınız");
+            String secim = Console.ReadLine();
+            Console.WriteLine("Lütfen başlangıç değerini giriniz");
+            int basdeger = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Lütfen son değerini giriniz");
+            int sondeger = Convert.ToInt32(Console.ReadLine());
+
+            if (sondeger != null)
+            {
+                if (secim == "Y" || secim == "y")
+                {
+                    for (int i = basdeger; i <= sondeger; i++)
+                    {
+                        Console.WriteLine(i);
+                    }
+                }
+
+                else if (secim == "N" || secim == "n")
+                {
+                    for (int i = basdeger; i >= sondeger; i++)
+                    {
+                        Console.WriteLine(i);
+                    }
+                }
+
+            }
+           
+            
+        }
+    }
+}
